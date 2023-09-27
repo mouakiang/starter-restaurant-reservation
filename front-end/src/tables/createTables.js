9ea2246333f96105c6ff2efffd3e1bf09f32ceca
Kia Moua <116847348+mouakiang@users.noreply.github.com>
2023-09-27 16:03:52 -0400

    added submit and cancel buttons as well as functionality of capacity being entered as a number

--- front-end/src/tables/createTables.js
+++ front-end/src/tables/createTables.js
@@ -1,79 +1,88 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import ErrorAlert from "../layout/ErrorAlert";
 import { createTable } from "../utils/api";
 
 function CreateTables() {
-
-//declare state
-const history = useHistory();
-const [error, setError] = useState(null);
-const [table, setTable] = useState({
+  const history = useHistory();
+  const [error, setError] = useState(null);
+  const [table, setTable] = useState({
     table_name: "",
-    capacity: 1,
-});
+    capacity: "", 
+  });
 
-//declare change handler
-const handleChange = ({target}) => {
+  const handleChange = ({ target }) => {
     setTable({
-        ...table,
-        [target.name]: target.value,
+      ...table,
+      [target.name]: target.value,
     });
-}
+  }
 
-//const handle submit
-const handleSubmit = (e) => {
+  async function handleSubmit(e) {
     e.preventDefault();
+  
+    const tableWithNumberCapacity = {
+      ...table,
+      capacity: parseFloat(table.capacity),
+    };
 
-    createTable(table)
-        .then(() => {
-            history.push(`/dashboard`);
-         })
-         .catch(setError);
-    }
+    createTable(tableWithNumberCapacity)
+      .then(() => {
+        history.push(`/dashboard`);
+      })
+      .catch(setError);
+  }
 
-    //return json
-return (
+  return (
     <>
-    <h1>Create A Table</h1>
-    <ErrorAlert error={error} />
-    <form onSubmit={handleSubmit} className="form-group">
+      <h1>Create A Table</h1>
+      <ErrorAlert error={error} />
+      <form onSubmit={handleSubmit} className="form-group">
         <div className="row mb-3">
-            <div className="col-4 form-group">
-                <label className="form-label" htmlFor="table_name">
-                    Table Name
-                </label>
-                <input
-                className="form-control"
-                id="table_name"
-                name="table_name"
-                type="text"
-                onChange={handleChange}
-                value={table.table_name}
-                />
-                <small className="form-text text-muted"> Enter First Name</small> 
-            </div>
-            <div className="col-4 form-group">
-                <label className="form-label" htmlFor="capacity">
-                    Capacity
-                </label>
-                <input 
-                className="form-control"
-                id="capacity"
-                name="capacity"
-                type="text"
-                onChange={handleChange}
-                required={true}
-                value={table.capacity}
-                />
-            </div>
+          <div className="col-4 form-group">
+            <label className="form-label" htmlFor="table_name">
+              Table Name
+            </label>
+            <input
+              className="form-control"
+              id="table_name"
+              name="table_name"
+              type="text"
+              onChange={handleChange}
+              required={true}
+              value={table.table_name}
+            />
+            <small className="form-text text-muted"> Enter Table Name</small>
+          </div>
+          <div className="col-4 form-group">
+            <label className="form-label" htmlFor="capacity">
+              Capacity
+            </label>
+            <input
+              className="form-control"
+              id="capacity"
+              name="capacity"
+              type="text" // Keep it as text
+              onChange={handleChange}
+              required={true}
+              value={table.capacity}
+            />
+            <small className="form-text text-muted">Enter Capacity</small>
+          </div>
         </div>
-    </form>
+        <button
+          type="button"
+          onClick={() => history.goBack()}
+          className="btn btn-secondary mr-2"
+        >
+          Cancel
+        </button>
+        <button type="submit" className="btn btn-primary">
+          Submit Table
+        </button>
+      </form>
     </>
-)
-
+  );
 }
-
-
 
 export default CreateTables;
